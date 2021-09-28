f2c147d7c247930b9b26b2217603b57adb505a94
aarti-2706 <72376227+aarti-2706@users.noreply.github.com>
2021-09-28 17:22:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+
+
+
+function setup(){
+  createCanvas(400,400)
+  var a=createSprite(200,200,20,20)
+  a.shapeColor='purple'
+ 
+}
+
+function draw(){
+  background("lightblue")
+  
+  drawSprites()
+
+}
